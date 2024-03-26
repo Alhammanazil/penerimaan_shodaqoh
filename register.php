847a917ae5e2c02b46f5fabe271dd3af5ce9d294
fauzulkhakim <studiopi7uaim@gmail.com>
2024-03-26 12:41:34 +0700

    show password login/register

--- register.php
+++ register.php
@@ -51,35 +51,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet" />
 
     <link rel="stylesheet" href="sweetalert2.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 
     <style>
-		body {
-			font-family: 'Poppins', sans-serif;
-		}
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
 
-		h1 {
-			text-align: center;
-			font-weight: 600;
-		}
-	</style>
-    
+        h1 {
+            text-align: center;
+            font-weight: 600;
+        }
+    </style>
+
 </head>
 
 <body>
     <div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh">
 
-        <form action="register.php" class="border shadow p-3 rounded"  method="post" style="width: 450px;" data-aos="fade-up" data-aos-duration="1000">
-        
+        <form action="register.php" class="border shadow p-3 rounded" method="post" style="width: 450px;" data-aos="fade-up" data-aos-duration="1000">
+
             <h1 class="text-center p-3">REGISTER</h1>
             <?php if (isset($_GET['error'])) { ?>
-            <div class="alert alert-danger" role="alert">
-                Registration failed. Please try again.
-            </div>
+                <div class="alert alert-danger" role="alert">
+                    Registration failed. Please try again.
+                </div>
             <?php } ?>
             <div class="mb-3">
                 <label for="name" class="form-label">Nama Lengkap</label>
@@ -89,23 +89,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <label for="username" class="form-label">Username</label>
                 <input type="text" class="form-control" name="username" id="username" required>
             </div>
-            <div class="mb-3">
+            <div class="mb-4">
                 <label for="password" class="form-label">Password</label>
                 <input type="password" name="password" class="form-control" id="password" required>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="show-password" onclick="showPassword()">
+                    <label class="form-check-label" for="show-password">
+                        Show Password
+                    </label>
+                </div>
             </div>
 
             <input type="hidden" name="role" value="user">
             <button type="submit" class="btn btn-primary">REGISTER</button>
-            
+
             <p class="mt-3">Sudah memiliki akun? <a href="index.php">Login</a></p>
         </form>
     </div>
 
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script src="sweetalert2.min.js"></script>
-    
+
     <script>
-    AOS.init();
+        AOS.init();
+
+        function showPassword() {
+            var x = document.getElementById("password");
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        }
     </script>
 
 </body>
