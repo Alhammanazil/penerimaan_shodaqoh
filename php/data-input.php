19f9f4fba6c1e47142d309579b9cfcadd690ab2f
fauzulkhakim <studiopi7uaim@gmail.com>
2024-03-27 13:35:46 +0700

    hapus pengecekan session

--- php/data-input.php
+++ php/data-input.php
@@ -1,9 +1,4 @@
-<?php  
+<?php
 
-if (isset($_SESSION['username']) && isset($_SESSION['id'])) {
-    
-    $sql = "SELECT * FROM input_detail";
-    $res = mysqli_query($conn, $sql);
-}else{
-	header("Location: index.php");
-} 
+$sql = "SELECT * FROM input_detail";
+$res = mysqli_query($conn, $sql);
